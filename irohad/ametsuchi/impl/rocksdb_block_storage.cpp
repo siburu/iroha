849db08d6b5d7faa6bffb6d52f55773bd2ea5cfc
iceseer <iceseer@gmail.com>
2021-10-01 12:17:50 +0300

    [RDB] in memory storage based on ring buffer
    Signed-off-by: iceseer <iceseer@gmail.com>

--- irohad/ametsuchi/impl/rocksdb_block_storage.cpp
+++ irohad/ametsuchi/impl/rocksdb_block_storage.cpp
@@ -9,6 +9,7 @@
 #include "backend/protobuf/block.hpp"
 #include "common/byteutils.hpp"
 #include "logger/logger.hpp"
+#include "ametsuchi/impl/in_memory_storage.hpp"
 
 using namespace iroha::ametsuchi;
 
@@ -42,24 +43,17 @@ RocksDbBlockStorage::RocksDbBlockStorage(
     logger::LoggerPtr log)
     : db_context_(std::move(db_context)),
       json_converter_(std::move(json_converter)),
+      storage_(std::make_shared<StorageType>()),
       log_(std::move(log)) {}
 
 bool RocksDbBlockStorage::insert(
     std::shared_ptr<const shared_model::interface::Block> block) {
   return json_converter_->serialize(*block).match(
       [&](const auto &block_json) {
-        RocksDbCommon common(db_context_);
-        CHECK_OPERATION("insertion",
-                        forBlock<kDbOperation::kCheck, kDbEntry::kMustNotExist>(
-                            common, block->height()));
-
-        common.valueBuffer() = block_json.value;
-        CHECK_OPERATION("storing",
-                        forBlock<kDbOperation::kPut>(common, block->height()));
-
-        CHECK_OPERATION("total count storing",
-                        incrementTotalBlocksCount(common));
-        return true;
+        return storage_.template exclusiveAccess([&](auto &storage) {
+          storage->insert(block->height(), block_json.value);
+          return true;
+        });
       },
       [this](const auto &error) {
         log_->warn("Error while block serialization: {}", error.error);
@@ -70,66 +64,57 @@ bool RocksDbBlockStorage::insert(
 boost::optional<std::unique_ptr<shared_model::interface::Block>>
 RocksDbBlockStorage::fetch(
     shared_model::interface::types::HeightType height) const {
-  RocksDbCommon common(db_context_);
-  if (auto result =
-          forBlock<kDbOperation::kGet, kDbEntry::kMustExist>(common, height);
-      expected::hasError(result)) {
-    log_->error("Error while block {} reading. Code: {}. Description: {}",
-                height,
-                result.assumeError().code,
-                result.assumeError().description);
-    return boost::none;
-  }
-
-  return json_converter_->deserialize(common.valueBuffer())
-      .match(
-          [&](auto &&block) {
-            return boost::make_optional<
-                std::unique_ptr<shared_model::interface::Block>>(
-                std::move(block.value));
-          },
-          [&](const auto &error)
-              -> boost::optional<
-                  std::unique_ptr<shared_model::interface::Block>> {
-            log_->warn("Error while block deserialization: {}", error.error);
-            return boost::none;
-          });
+  return storage_.sharedAccess(
+      [&](auto const &storage)
+          -> boost::optional<std::unique_ptr<shared_model::interface::Block>> {
+        if (auto data = storage->find(height)) {
+          return json_converter_->deserialize((*data).get())
+              .match(
+                  [&](auto &&block) {
+                    return boost::make_optional<
+                        std::unique_ptr<shared_model::interface::Block>>(
+                        std::move(block.value));
+                  },
+                  [&](const auto &error)
+                      -> boost::optional<
+                          std::unique_ptr<shared_model::interface::Block>> {
+                    log_->warn("Error while block deserialization: {}",
+                               error.error);
+                    return boost::none;
+                  });
+        }
+        log_->error("Error while block {} reading.", height);
+        return boost::none;
+      });
 }
 
 size_t RocksDbBlockStorage::size() const {
-  RocksDbCommon common(db_context_);
-  if (auto result =
-          forBlocksTotalCount<kDbOperation::kGet, kDbEntry::kMustExist>(common);
-      expected::hasValue(result))
-    return *result.assumeValue();
-  return 0ull;
+  return storage_.sharedAccess([](auto const &storage){
+    return storage->allTimeValues();
+  });
 }
 
 void RocksDbBlockStorage::reload() {}
 
 void RocksDbBlockStorage::clear() {
-  RocksDbCommon common(db_context_);
-
-  if (auto status = common.filterDelete(fmtstrings::kPathWsv); !status.ok())
-    log_->error("Unable to delete WSV. Description: {}", status.ToString());
-
-  if (auto status = common.filterDelete(fmtstrings::kPathStore); !status.ok())
-    log_->error("Unable to delete STORE. Description: {}", status.ToString());
+  storage_.exclusiveAccess([](auto &storage) { storage->clear(); });
 }
 
 iroha::expected::Result<void, std::string> RocksDbBlockStorage::forEach(
     iroha::ametsuchi::BlockStorage::FunctionType function) const {
-  uint64_t const blocks_count = size();
-  for (uint64_t ix = 1; ix <= blocks_count; ++ix) {
-    auto maybe_block = fetch(ix);
-    if (maybe_block) {
-      auto maybe_error = function(std::move(maybe_block).value());
-      if (iroha::expected::hasError(maybe_error)) {
-        return maybe_error.assumeError();
-      }
-    } else {
-      return fmt::format("Failed to fetch block {}", ix);
-    }
-  }
-  return {};
+  return storage_.sharedAccess(
+      [&](auto const &storage) -> iroha::expected::Result<void, std::string> {
+        storage->forEach([&](auto const & /*key*/, auto const &value) {
+          json_converter_->deserialize(value).match(
+              [&](auto &&block) {
+                function(std::shared_ptr<const shared_model::interface::Block>(
+                    std::move(block.value)));
+              },
+              [&](const auto &error) {
+                log_->warn("Error while block deserialization: {}",
+                           error.error);
+              });
+        });
+        return {};
+      });
 }
